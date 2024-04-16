e6bda0efa67288b83c8d98c823b3aab2b7954df9
Chris Pickel <sfiera@twotaled.com>
2024-04-16 16:15:52 +0900

    Decode many strings in bank $1

---
+++ misc/decode.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+import sys
+
+charmap = {}
+for base, chars in {
+    0x00: "\n\r",
+    0x0e: "»«",
+    0x20: " !\"#$%&'()*+,-./",
+    0x30: "0123456789",
+    0x3a: ":;<=>?@",
+    0x41: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    0x5b: "[\\]^_`",
+    0x61: "abcdefghijklmnopqrstuvwxyz",
+    0x7b: "{|}",
+    0xa6: "ｦｧｨｩｪｫｬｭｮｯｰ",
+    0xb1: "ｱｲｳｴｵｶｷｸｹｺｻｼｽｾｿﾀﾁﾂﾃﾄﾅﾆﾇﾈﾉﾊﾋﾌﾍﾎﾏﾐﾑﾒﾓﾔﾕﾖﾗﾘﾙﾚﾛﾜﾝﾞﾟ",
+}.items():
+    for i, ch in enumerate(chars):
+        charmap[base + i] = ch
+
+data = sys.stdin.read().split("$")
+print("    db \"", end="", flush=True)
+for part in data[1:]:
+    byte = int(part[:2], base=16)
+    char = charmap[byte]
+    if char < " ":
+        char = repr(char)[1:-1]
+    print(char, end="", flush=True)
+    if byte == 0:
+        print("\"\n    db \"", end="", flush=True)
+print('"')
