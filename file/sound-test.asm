e083920460a76aec3d5c5d7d77027a9da9de7ab1
Chris Pickel <sfiera@twotaled.com>
2024-06-07 14:34:37 +0900

    Disassemble sound-test.asm code

--- file/sound-test.asm
+++ file/sound-test.asm
@@ -24,40 +24,423 @@ Icon::
     INCBIN "gfx/icon/sound-test.2bpp"
 
 Main::
-    db $3e, $c4, $01, $01, $02, $11, $12, $0d, $26, $04, $cf, $b3, $cf, $11, $21, $00
-    db $c6, $d7, $06, $00, $d7, $77, $00, $c9, $7a, $22, $af, $22, $7b, $22, $af, $22
-    db $3e, $0f, $22, $af, $22, $3e, $07, $22, $22, $3e, $01, $22, $22, $af, $22, $d7
-    db $06, $00, $d7, $ea, $00, $c9, $d7, $06, $00, $d7, $22, $01, $c9, $d7, $06, $00
-    db $d7, $36, $01, $c9, $d7, $06, $00, $d7, $4a, $01, $c9, $d7, $06, $00, $d7, $5e
-    db $01, $c9, $d7, $06, $00, $d7, $72, $01, $c9, $cf, $b1, $11, $1b, $01, $01, $09
-    db $1a, $2e, $06, $3e, $00, $26, $37, $cf, $bf, $cb, $4c, $20, $1d, $cb, $54, $20
-    db $19, $c5, $d5, $e5, $f5, $d7, $06, $00, $d7, $1f, $00, $c9, $cf, $d8, $c1, $e6
-    db $37, $e1, $67, $d1, $78, $c1, $28, $dd, $18, $df, $cf, $01, $fa, $14, $00, $11
-    db $38, $1b, $fe, $01, $c8, $cf, $1a, $c9, $fe, $04, $20, $0d, $cb, $44, $c8, $cf
-    db $18, $d7, $06, $00, $d7, $53, $01, $c9, $c9, $cb, $44, $28, $28, $d7, $06, $00
-    db $d7, $07, $00, $c9, $d7, $43, $01, $e9, $d7, $a3, $00, $e1, $b7, $c8, $d7, $b8
-    db $00, $e1, $3d, $c8, $d7, $cd, $00, $e1, $3d, $c8, $d7, $e2, $00, $e1, $3d, $c8
-    db $d7, $f7, $00, $e1, $c9, $4c, $d7, $98, $00, $e1, $11, $00, $c6, $b7, $28, $25
-    db $d7, $a9, $00, $e1, $11, $02, $c6, $3d, $28, $1b, $d7, $ba, $00, $e1, $11, $04
-    db $c6, $3d, $28, $11, $d7, $cb, $00, $e1, $11, $06, $c6, $3d, $28, $07, $d7, $e1
-    db $00, $e1, $11, $08, $c6, $1a, $13, $47, $cb, $61, $20, $0a, $1a, $b7, $28, $01
-    db $3d, $12, $d7, $e5, $00, $e9, $1a, $b8, $28, $f7, $3c, $18, $f4, $11, $03, $01
-    db $d7, $0a, $01, $e1, $cf, $6a, $21, $01, $01, $cf, $b8, $d7, $ef, $00, $e1, $cf
-    db $69, $21, $0a, $00, $cf, $b8, $d7, $49, $01, $e1, $cf, $69, $d7, $06, $00, $d7
-    db $22, $00, $c9, $d7, $06, $00, $d7, $36, $00, $c9, $d7, $06, $00, $d7, $4a, $00
-    db $c9, $d7, $06, $00, $d7, $5e, $00, $c9, $d7, $7a, $00, $c9, $fa, $01, $c6, $cf
-    db $13, $c9, $21, $03, $0f, $fa, $00, $c6, $d7, $06, $00, $d7, $7a, $00, $c9, $21
-    db $03, $0b, $fa, $01, $c6, $18, $6f, $fa, $03, $c6, $cf, $14, $c9, $21, $04, $0f
-    db $fa, $02, $c6, $d7, $06, $00, $d7, $5f, $00, $c9, $21, $04, $0b, $fa, $03, $c6
-    db $18, $54, $fa, $05, $c6, $cf, $15, $c9, $21, $05, $0f, $fa, $04, $c6, $d7, $06
-    db $00, $d7, $44, $00, $c9, $21, $05, $0b, $fa, $05, $c6, $18, $39, $fa, $07, $c6
-    db $cf, $19, $c9, $21, $06, $0f, $fa, $06, $c6, $d7, $06, $00, $d7, $29, $00, $c9
-    db $21, $06, $0b, $fa, $07, $c6, $18, $1e, $fa, $09, $c6, $87, $87, $67, $2e, $bc
-    db $cf, $cb, $c9, $21, $08, $0f, $fa, $08, $c6, $d7, $06, $00, $d7, $09, $00, $c9
-    db $21, $08, $0b, $fa, $09, $c6, $f5, $cf, $b8, $f1, $5f, $16, $00, $21, $00, $c4
-    db $cf, $a3, $21, $03, $c4, $cf, $69, $c9, $21, $0a, $04, $cf, $b8, $cf, $16, $d7
-    db $06, $00, $d7, $0a, $00, $c9, $21, $0a, $0e, $cf, $b8, $cf, $17, $d7, $85, $00
-    db $e1, $b7, $28, $04, $d7, $83, $00, $e1, $cf, $69, $c9
+    ld a, $c4
+    ld bc, $0201
+    ld de, $0d12
+    ld h, $04
+    rst $08
+    or e
+    rst $08
+    ld de, $0021
+    add $d7
+    ld b, $00
+    rst $10
+    ld [hl], a
+    nop
+    ret
+
+    ld a, d
+    ld [hli], a
+    xor a
+    ld [hli], a
+    ld a, e
+    ld [hli], a
+    xor a
+    ld [hli], a
+    ld a, $0f
+    ld [hli], a
+    xor a
+    ld [hli], a
+    ld a, $07
+    ld [hli], a
+    ld [hli], a
+    ld a, $01
+    ld [hli], a
+    ld [hli], a
+    xor a
+    ld [hli], a
+    rst $10
+    ld b, $00
+    rst $10
+    ld [$c900], a
+    rst $10
+    ld b, $00
+    rst $10
+    ld [hli], a
+    ld bc, $d7c9
+    ld b, $00
+    rst $10
+    ld [hl], $01
+    ret
+
+    rst $10
+    ld b, $00
+    rst $10
+    ld c, d
+    ld bc, $d7c9
+    ld b, $00
+    rst $10
+    ld e, [hl]
+    ld bc, $d7c9
+    ld b, $00
+    rst $10
+    ld [hl], d
+    ld bc, $cfc9
+    or c
+    ld de, $011b
+    ld bc, $1a09
+    ld l, $06
+    ld a, $00
+
+jr_0134:
+    ld h, $37
+    rst $08
+    cp a
+
+jr_0138:
+    bit 1, h
+    jr nz, jr_0159
+    bit 2, h
+    jr nz, jr_0159
+    push bc
+    push de
+    push hl
+    push af
+    rst $10
+    ld b, $00
+    rst $10
+
+jr_0148:
+    rra
+    nop
+    ret
+
+    rst $08
+    ret c
+
+    pop bc
+    and $37
+    pop hl
+    ld h, a
+    pop de
+    ld a, b
+    pop bc
+    jr z, jr_0134
+    jr jr_0138
+
+jr_0159:
+    rst $08
+    ld bc, $14fa
+    nop
+    ld de, $1b38
+    cp $01
+    ret z
+
+    rst $08
+    ld a, [de]
+    ret
+
+    cp $04
+    jr nz, jr_0178
+    bit 0, h
+    ret z
+
+    rst $08
+    jr jr_0148
+    ld b, $00
+    rst $10
+    ld d, e
+    ld bc, $c9c9
+
+jr_0178:
+    bit 0, h
+    jr z, jr_01a4
+    rst $10
+    ld b, $00
+    rst $10
+    rlca
+    nop
+    ret
+
+    rst $10
+    ld b, e
+    ld bc, $d7e9
+    and e
+    nop
+    pop hl
+    or a
+    ret z
+
+    rst $10
+    cp b
+    nop
+    pop hl
+    dec a
+    ret z
+
+    rst $10
+    call $e100
+    dec a
+    ret z
+
+    rst $10
+    ldh [c], a
+    nop
+    pop hl
+    dec a
+    ret z
+
+    rst $10
+    rst $30
+    nop
+    pop hl
+    ret
+
+jr_01a4:
+    ld c, h
+    rst $10
+    sbc b
+    nop
+    pop hl
+    ld de, $c600
+    or a
+    jr z, jr_01d4
+    rst $10
+    xor c
+    nop
+    pop hl
+    ld de, $c602
+    dec a
+    jr z, jr_01d4
+    rst $10
+    cp d
+    nop
+    pop hl
+    ld de, $c604
+    dec a
+    jr z, jr_01d4
+    rst $10
+    rlc b
+    pop hl
+    ld de, $c606
+    dec a
+    jr z, jr_01d4
+    rst $10
+    pop hl
+    nop
+    pop hl
+    ld de, $c608
+
+jr_01d4:
+    ld a, [de]
+    inc de
+    ld b, a
+    bit 4, c
+    jr nz, jr_01e5
+    ld a, [de]
+    or a
+    jr z, jr_01e0
+    dec a
+
+jr_01e0:
+    ld [de], a
+    rst $10
+    push hl
+    nop
+    jp hl
+
+jr_01e5:
+    ld a, [de]
+    cp b
+    jr z, jr_01e0
+    inc a
+    jr jr_01e0
+    ld de, $0103
+    rst $10
+    ld a, [bc]
+    ld bc, $cfe1
+    ld l, d
+    ld hl, $0101
+    rst $08
+    cp b
+    rst $10
+    rst $28
+    nop
+    pop hl
+    rst $08
+    ld l, c
+    ld hl, $000a
+    rst $08
+    cp b
+    rst $10
+    ld c, c
+    ld bc, $cfe1
+    ld l, c
+    rst $10
+    ld b, $00
+    rst $10
+    ld [hli], a
+    nop
+    ret
+
+    rst $10
+    ld b, $00
+    rst $10
+    ld [hl], $00
+    ret
+
+    rst $10
+    ld b, $00
+    rst $10
+    ld c, d
+    nop
+    ret
+
+    rst $10
+    ld b, $00
+    rst $10
+    ld e, [hl]
+    nop
+    ret
+
+    rst $10
+    ld a, d
+    nop
+    ret
+
+    ld a, [$c601]
+    rst $08
+    inc de
+    ret
+
+    ld hl, $0f03
+    ld a, [$c600]
+    rst $10
+    ld b, $00
+    rst $10
+    ld a, d
+    nop
+    ret
+
+    ld hl, $0b03
+    ld a, [$c601]
+    jr jr_02b5
+    ld a, [$c603]
+    rst $08
+    inc d
+    ret
+
+    ld hl, $0f04
+    ld a, [$c602]
+    rst $10
+    ld b, $00
+    rst $10
+    ld e, a
+    nop
+    ret
+
+    ld hl, $0b04
+    ld a, [$c603]
+    jr jr_02b5
+    ld a, [$c605]
+    rst $08
+    dec d
+    ret
+
+    ld hl, $0f05
+    ld a, [$c604]
+    rst $10
+    ld b, $00
+    rst $10
+    ld b, h
+    nop
+    ret
+
+    ld hl, $0b05
+    ld a, [$c605]
+    jr jr_02b5
+    ld a, [$c607]
+    rst $08
+    add hl, de
+    ret
+
+    ld hl, $0f06
+    ld a, [$c606]
+    rst $10
+    ld b, $00
+    rst $10
+    add hl, hl
+    nop
+    ret
+
+    ld hl, $0b06
+    ld a, [$c607]
+    jr jr_02b5
+    ld a, [$c609]
+    add a
+    add a
+    ld h, a
+    ld l, $bc
+    rst $08
+    set 1, c
+    ld hl, $0f08
+    ld a, [$c608]
+    rst $10
+    ld b, $00
+    rst $10
+    add hl, bc
+    nop
+    ret
+
+    ld hl, $0b08
+    ld a, [$c609]
+
+jr_02b5:
+    push af
+    rst $08
+    cp b
+    pop af
+    ld e, a
+    ld d, $00
+    ld hl, $c400
+    rst $08
+    and e
+    ld hl, $c403
+    rst $08
+    ld l, c
+    ret
+
+    ld hl, $040a
+    rst $08
+    cp b
+    rst $08
+    ld d, $d7
+    ld b, $00
+    rst $10
+    ld a, [bc]
+    nop
+    ret
+
+    ld hl, $0e0a
+    rst $08
+    cp b
+    rst $08
+    rla
+    rst $10
+    add l
+    nop
+    pop hl
+    or a
+    jr z, jr_02e7
+    rst $10
+    add e
+    nop
+    pop hl
+
+jr_02e7:
+    rst $08
+    ld l, c
+    ret
 
 Interface:
     db "SOUND TEST ROOM\n"
