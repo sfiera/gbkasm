885c80a9039c269d7af0fdd8709483b95cfc82c1
Chris Pickel <sfiera@twotaled.com>
2024-06-22 17:23:54 +0900

    Disassemble chardump.asm

--- file/chardump.asm
+++ file/chardump.asm
@@ -18,22 +18,176 @@ Title::
     dk "CHAR-DUMP"
 
 Main::
-    db $31, $00, $e0, $af, $cf, $b3, $af, $21, $05, $cb, $22, $77, $ea, $04, $cb, $3e
-    db $80, $21, $10, $01, $11, $05, $02, $01, $08, $10, $cf, $59, $d7, $06, $00, $d7
-    db $4f, $00, $c9, $d7, $06, $00, $d7, $7f, $00, $c9, $cf, $b1, $cf, $d8, $21, $05
-    db $cb, $4f, $cb, $59, $20, $60, $cb, $71, $20, $20, $cb, $79, $20, $26, $cb, $69
-    db $20, $1c, $cb, $61, $20, $0e, $cb, $41, $20, $3a, $cb, $49, $20, $40, $cb, $51
-    db $28, $d8, $cf, $01, $7e, $ee, $80, $77, $20, $0c, $23, $34, $18, $08, $7e, $ee
-    db $80, $77, $28, $02, $23, $35, $d7, $06, $00, $d7, $05, $00, $c9, $18, $bb, $21
-    db $02, $04, $cf, $b8, $21, $05, $cb, $5e, $23, $56, $21, $c4, $ca, $cf, $a4, $2b
-    db $2b, $cf, $69, $c9, $3e, $80, $86, $22, $3e, $00, $8e, $77, $18, $08, $7e, $d6
-    db $80, $22, $7e, $de, $00, $77, $d7, $06, $00, $d7, $d5, $ff, $c9, $d7, $06, $00
-    db $d7, $05, $00, $c9, $18, $84, $21, $05, $cb, $fa, $05, $cb, $cb, $37, $4f, $06
-    db $08, $11, $05, $00, $c5, $d7, $06, $00, $d7, $3b, $00, $c9, $1c, $0c, $05, $20
-    db $f4, $c1, $11, $04, $02, $cb, $31, $06, $10, $d7, $06, $00, $d7, $27, $00, $c9
-    db $14, $0c, $05, $20, $f4, $cf, $b5, $1e, $00, $21, $00, $88, $01, $00, $08, $cf
-    db $a6, $21, $05, $cb, $5e, $23, $56, $01, $03, $80, $21, $00, $88, $cf, $1b, $3e
-    db $03, $cf, $b4, $c9, $d5, $c5, $6b, $62, $cf, $b8, $79, $e6, $0f, $fe, $0a, $38
-    db $02, $c6, $07, $c6, $30, $cf, $bb, $c1, $d1, $c9,
+    ld sp, $e000
+    xor a
+    trap DrawInit
+    xor a
+    ld hl, varAddr
+    ld [hli], a
+    ld [hl], a
+    ld [varUnknown], a
+    ld a, $80
+    ld hl, $0110
+    ld de, $0205
+    ld bc, $1008
+    trap $59
+    callx DrawAddress
+    callx DrawGrid
+
+.loop
+    trap AwaitFrame
+    trap $d8
+    ld hl, varAddr
+    ld c, a
+    bit BtnSta, c
+    jr nz, Main2.start
+    bit BtnUp, c
+    jr nz, .up
+    bit BtnDn, c
+    jr nz, .dn
+    bit BtnLt, c
+    jr nz, .lt
+    bit BtnRt, c
+    jr nz, .rt
+    bit BtnA, c
+    jr nz, Main2.a
+    bit BtnB, c
+    jr nz, Main2.b
+    bit BtnSel, c
+    jr z, .loop
+    trap ExitToMenu
+
+.rt
+    ld a, [hl]
+    xor $80
+    ld [hl], a
+    jr nz, .newAddr
+.up
+    inc hl
+    inc [hl]
+    jr .newAddr
+
+.lt
+    ld a, [hl]
+    xor $80
+    ld [hl], a
+    jr z, .newAddr
+.dn
+    inc hl
+    dec [hl]
+
+.newAddr
+    callx DrawAddress
+    jr .loop
+
+
+DrawAddress:
+    ld hl, $0402
+    trap MoveCursor
+    ld hl, varAddr
+    ld e, [hl]
+    inc hl
+    ld d, [hl]
+    ld hl, varAddrStr
+    trap IntToStringHex
+    dec hl
+    dec hl
+    trap DrawString
+    ret
+
+Main2:
+.a
+    ld a, $80
+    add [hl]
+    ld [hli], a
+    ld a, $00
+    adc [hl]
+    ld [hl], a
+    jr .start
+.b
+    ld a, [hl]
+    sub $80
+    ld [hli], a
+    ld a, [hl]
+    sbc $00
+    ld [hl], a
+.start
+    callx DrawAddress
+    callx DrawGrid
+    jr Main.loop
+
+DrawGrid:
+    ld hl, varAddr
+    ld a, [varAddr]
+    swap a
+    ld c, a
+    ld b, $08
+    ld de, $0005
+    push bc
+
+.nextRow
+    callx DrawDigit
+    inc e
+    inc c
+    dec b
+    jr nz, .nextRow
+
+    pop bc
+    ld de, $0204
+    swap c
+    ld b, $10
+
+.nextCol
+    callx DrawDigit
+    inc d
+    inc c
+    dec b
+    jr nz, .nextCol
+
+    trap $b5
+    ld e, $00
+    ld hl, $8800
+    ld bc, $0800
+    trap $a6
+    ld hl, varAddr
+    ld e, [hl]
+    inc hl
+    ld d, [hl]
+    ld bc, $8003
+    ld hl, $8800
+    trap $1b
+    ld a, $03
+    trap $b4
+    ret
+
+DrawDigit:
+    push de
+    push bc
+    ld l, e
+    ld h, d
+    trap MoveCursor
+    ld a, c
+    and $F
+    cp $A
+    jr c, .decimal
+    add "A" - $A - "0"
+.decimal
+    add "0"
+    trap DrawChar
+    pop bc
+    pop de
+    ret
 
 End::
+
+SECTION "WRAM Variables", WRAM0[$cb04]
+
+varUnknown:
+    db
+varAddr:
+    dw
+
+SECTION "WRAM Scratch", WRAM0[$cac4]
+
+varAddrStr:
+    ds 4
