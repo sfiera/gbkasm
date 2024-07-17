51ede782edb521c949e6051e3e10bf7342d046ee
Chris Pickel <sfiera@twotaled.com>
2024-07-17 12:34:22 +0900

    Improve comments in saita.asm

--- file/saita.asm
+++ file/saita.asm
@@ -33,12 +33,12 @@ POPC
 
     db Pattern.end - Pattern
 Pattern:
-    db $40, $f7, $a7                           ; unknown pattern start
+    db $40, $f7, $a7                           ; pattern start, tempo, 1=C4
     db $31, $33, $35, $83, $31, $33, $35, $83  ; c d e - c d e -
     db $38, $35, $33, $31, $33, $35, $33, $83  ; g e d c d e d -
     db $31, $33, $35, $83, $31, $33, $35, $83  ; c d e - c d e -
     db $38, $35, $33, $31, $33, $35, $31, $83  ; g e d c d e c -
     db $38, $38, $35, $38, $3a, $3a, $38, $83  ; g g e g a a g -
     db $35, $35, $33, $33, $51, $83            ; e e d d c2 -
-    db $b0                                     ; unknown pattern end
+    db $b0                                     ; pattern end
 .end
