1e4656c2fba8a888a5b15da7b7f1a13f8e064a71
Chris Pickel <sfiera@twotaled.com>
2024-08-02 18:33:17 +0900

    Include fragments in trap report

--- tools/trap_report.sh
+++ tools/trap_report.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-grep -o '^\s*trap \$..\|call trap_..' file/*.asm bank_*.inc \
+find src -name '*.asm' -or -name '*.inc' \
+    | xargs grep -o '^\s*trap \$..\|call trap_..' \
     | sort \
     | uniq -c \
     | sed -E 's/^ *([0-9]*) ([^:]*):.*(trap \$|call trap_)(.*)/$\4\t\2\t\1/'
