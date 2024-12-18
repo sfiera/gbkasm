cd9a6a8cc8437aeb3b00525b1a4a1b74c75c9b41
Chris Pickel <sfiera@twotaled.com>
2024-12-18 20:30:37 +0900

    Break up Binary Land sprite sheet

--- src/frag/bland/sprites.asm
+++ src/frag/bland/sprites.asm
@@ -1,3 +1,6 @@
 SECTION "Fragment", ROM0[$0]
 
-    INCBIN "gfx/bland/sprites.2bpp"
+    INCBIN "gfx/bland/chars.2bpp",16
+    INCBIN "gfx/bland/foes.2bpp",16
+    INCBIN "gfx/bland/misc.2bpp",16
+    INCBIN "gfx/bland/powerups.2bpp"
