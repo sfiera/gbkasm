eeb03285eab509d6ce33f948366e0ae5a078ec33
Chris Pickel <sfiera@twotaled.com>
2024-12-19 14:05:13 +0900

    Split and rename Magnets images

--- src/frag/magnets/bin1.asm
+++ src/frag/magnets/bin1.asm
@@ -1,3 +1,4 @@
 SECTION "Fragment", ROM0[$0]
 
-    INCBIN "src/gfx/magnets/bin1.2bpp"
+    INCBIN "src/gfx/magnets/player1.2bpp"
+    INCBIN "src/gfx/magnets/player2.2bpp"
--- src/frag/magnets/bin3.asm
+++ src/frag/magnets/bin3.asm
@@ -1,3 +1,3 @@
 SECTION "Fragment", ROM0[$0]
 
-    INCBIN "src/gfx/magnets/bin3.2bpp"
+    INCBIN "src/gfx/magnets/splash.2bpp"
