d44d778150ee3ebba90eb0d4b5814f05cb7c0a48
Chris Pickel <sfiera@twotaled.com>
2024-12-23 09:19:37 -0800

    Remove src/ from included paths

--- src/frag/magnets/bin2.asm
+++ src/frag/magnets/bin2.asm
@@ -1,4 +1,4 @@
 SECTION "Fragment", ROM0[$0]
 
-    INCBIN "src/gfx/magnets/foes1.2bpp"
-    INCBIN "src/gfx/magnets/foes2.2bpp"
+    INCBIN "gfx/magnets/foes1.2bpp"
+    INCBIN "gfx/magnets/foes2.2bpp"
--- src/frag/magnets/bin3.asm
+++ src/frag/magnets/bin3.asm
@@ -1,3 +1,3 @@
 SECTION "Fragment", ROM0[$0]
 
-    INCBIN "src/gfx/magnets/splash.2bpp"
+    INCBIN "gfx/magnets/splash.2bpp"
