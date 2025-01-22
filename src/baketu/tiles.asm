a4b607f66ab6277ed62fd95988e68ec72bca8088
Chris Pickel <sfiera@twotaled.com>
2025-01-22 17:48:41 +0900

    Split up Bakechu Relay tiles and interpret tilemap

--- src/baketu/tiles.asm
+++ src/baketu/tiles.asm
@@ -1,3 +1,6 @@
 SECTION "Fragment", ROM0[$0]
 
-    INCBIN "baketu/tiles.2bpp"
+    INCBIN "baketu/wall.2bpp"
+    INCBIN "baketu/mouse.2bpp", 16
+    INCBIN "baketu/splat.2bpp", 16
+    INCBIN "baketu/wall.tilemap", 32
